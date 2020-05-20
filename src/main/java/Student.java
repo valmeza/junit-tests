19c6858eb149e12bb1f87ee91a66290b1cb03f19
Valeria Meza <valeriameza173@gmail.com>
2020-05-20 14:18:46 -0500

    completed the first part of the tdd exercise.

---
+++ src/main/java/Student.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Student {
+    long id;
+    String name;
+    ArrayList<Integer> grades;
+
+    public Student(long id, String name) {
+        this.name = name;
+        this.id = id;
+        this.grades = new ArrayList<>();
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return this.grades;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        double sum = 0;
+        for(int grade : this.getGrades()) {
+            sum += grade;
+        }
+        return sum / this.getGrades().size();
+    }
+}
---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.lang.model.util.Types;
+
+import static org.junit.Assert.*;
+
+public class StudentTest {
+
+    @Test
+    public void idTest() {
+        Student valeria = new Student(1, "Valeria");
+
+        assertEquals("Valeria", valeria.getName());
+        assertEquals(1, valeria.getId());
+    }
+
+    @Test
+    public void addGradeTest() {
+        Student valeria = new Student(1, "Valeria");
+        valeria.addGrade(90);
+        assertSame(90, valeria.getGrades().get(0));
+    }
+
+    @Test
+    public void gradeAverageTest() {
+        Student valeria = new Student(1, "Valeria");
+        valeria.addGrade(90);
+        assertEquals(90, valeria.getGradeAverage(), 0);
+    }
+}
