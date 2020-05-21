d6b794d93ac6b0a12fcffb3de880e722e01d4ed6
Valeria Meza <valeriameza173@gmail.com>
2020-05-21 16:46:28 -0500

    completed legacy code exercise

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -1,45 +1,37 @@
-import org.junit.Before;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 import static org.junit.Assert.*;
 
 public class CohortTest {
 
-    Cohort emptyCohort;
-    Cohort cohortWithOne;
-    Cohort cohortWithMany;
+    @Test
+    public void testIfAddStudentWorks() {
+        Cohort apex = new Cohort();
+        Student val = new Student(1L, "Valeria");
+        apex.addStudent(val);
+        assertEquals(1L,apex.getStudents().get(0).getId());
 
-    @Before
-    public void setup(){
-        emptyCohort = new Cohort();
-        cohortWithOne = new Cohort();
-        cohortWithMany = new Cohort();
-
-        Student fer = new Student(1, "Fer M");
-        fer.addGrade(90);
-
-        Student trant = new Student(2, "Trant B");
-        trant.addGrade(100);
-        trant.addGrade(100);
-        trant.addGrade(100);
-
-        cohortWithOne.addStudent(fer);
-        cohortWithMany.addStudent(fer);
-        cohortWithMany.addStudent(trant);
+        // Test name
+        assertEquals("Valeria", apex.getStudents().get(0).getName());
     }
 
     @Test
-    public void testAddStudentAndGetStudentsWork(){
-        assertEquals(0, emptyCohort.getStudents().size());
-        assertEquals(1, cohortWithOne.getStudents().size());
-//        assertEquals(1, cohortWithOne.getStudents().get(0).getId());
+    public void testIfGetStudentsWorks() {
+        // make sure it zero at the start
+        Cohort apex = new Cohort();
+        assertEquals(0, apex.getStudents().size());
+
+        Student val = new Student(2L, "Valeria");
+        apex.addStudent(val);
+        assertEquals(1, apex.getStudents().size());
     }
 
     @Test
-    public void testIfAvgIsCorrect(){
-        assertEquals(95.0, cohortWithMany.getCohortAverage(), 0);
+    public void testIfAverageWorks() {
+        Cohort apex = new Cohort();
+        Student val = new Student(2L, "Valeria");
+        val.addGrade(100);
+        val.addGrade(50);
+        apex.addStudent(val);
+        assertEquals(75, apex.getCohortAverage(), 0);
     }
-
 }
